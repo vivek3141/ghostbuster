fa14aabb1a78556a8f458fe154899c162bb18429
vivek3141 <vivnps.verma@gmail.com>
2023-05-18 03:24:48 +0000

    Fixing utils __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 import openai
 from .write_logprobs import *
+from .featurize import *
+from .n_gram import *
 
 openai_config = json.loads(
     open("/home/nickatomlin/vivek/detect_ai/openai.config").read()
