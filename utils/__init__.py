c5ebddab5a3a8544c0485cfc87d0572e8e68bb30
vivek3141 <vivnps.verma@gmail.com>
2023-05-26 07:11:58 +0000

    Updating README

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,8 +3,17 @@ from .write_logprobs import *
 from .featurize import *
 from .n_gram import *
 
-openai_config = json.loads(
-    open("/home/nickatomlin/vivek/detect_ai/openai.config").read()
-)
-openai.api_key = openai_config["api_key"]
-openai.organization = openai_config["organization"]
+openai_path = ""
+if os.path.exists("../../openai.config"):
+    openai_path = "../../openai.config"
+elif os.path.exists("../openai.config"):
+    openai_path = "../openai.config"
+elif os.path.exists("openai.config"):
+    openai_path = "openai.config"
+
+if openai_path:
+    openai_config = json.loads(
+        open("/home/nickatomlin/vivek/detect_ai/openai.config").read()
+    )
+    openai.api_key = openai_config["api_key"]
+    openai.organization = openai_config["organization"]
