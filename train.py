d77b7deb26b635c46964cf1b86730552b68fad67
Vivek Verma <vivnps.verma@gmail.com>
2023-08-22 05:39:49 +0900

    New train.py file

--- train.py
+++ train.py
@@ -1,138 +1,121 @@
+import argparse
+import math
 import numpy as np
-import tiktoken
-import dill as pickle
-
-from writing_prompts.data.load import generate_dataset as wp_generate_dataset
-from reuter.data.load import generate_dataset as reuter_generate_dataset
-from essay.data.load import generate_dataset as essay_generate_dataset
-
-from utils.featurize import t_featurize, normalize
-from utils.symbolic import get_words, vec_functions, scalar_functions, get_all_logprobs
+import os
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 
-np.random.seed(0)
+from tabulate import tabulate
 
-WP_PATH = "writing_prompts"
-REUTER_PATH = "reuter/data"
-ESSAY_PATH = "essay"
-
-best_features = open("model/best_features.txt").read().strip().split("\n")
-
-
-def get_exp_featurize(vector_map, best_features):
-    def calc_features(file, exp):
-        exp_tokens = get_words(exp)
-        curr = vector_map[exp_tokens[0]](file)
-
-        for i in range(1, len(exp_tokens)):
-            if exp_tokens[i] in vec_functions:
-                next_vec = vector_map[exp_tokens[i+1]](file)
-                curr = vec_functions[exp_tokens[i]](curr, next_vec)
-            elif exp_tokens[i] in scalar_functions:
-                return scalar_functions[exp_tokens[i]](curr)
-
-    def exp_featurize(file):
-        return np.array([calc_features(file, exp) for exp in best_features])
-
-    return exp_featurize
+from utils.featurize import normalize, t_featurize
+from utils.symbolic import get_all_logprobs, train_trigram, get_exp_featurize
+from utils.symbolic import generate_symbolic_data
+from utils.load import get_generate_dataset, Dataset
 
 
-# Generate WP Data
-wp_symbolic_data = pickle.load(open(f"writing_prompts/symbolic_data", "rb"))
-wp_t = wp_generate_dataset(t_featurize, base_dir=f"{WP_PATH}/", verbose=True)
-wp_data = np.concatenate(
-    [wp_symbolic_data[e]
-     for e in best_features] + [wp_t], axis=1
-)
-wp_labels = wp_generate_dataset(
-    lambda file: 1 if "gpt" in file else 0, base_dir=f"{WP_PATH}/")
+with open("model/best_features.txt") as f:
+    best_features_all = f.read().strip().split("\n")
 
-# Generate Reuter Train Data
-reuter_train_symbolic_data = pickle.load(
-    open(f"reuter/symbolic_data", "rb"))
-reuter_train_t = reuter_generate_dataset(
-    t_featurize, "train", base_dir=f"{REUTER_PATH}/", verbose=True)
-reuter_train_data = np.concatenate(
-    [reuter_train_symbolic_data[e]
-        for e in best_features] + [reuter_train_t], axis=1
-)
-reuter_train_labels = reuter_generate_dataset(
-    lambda file: 1 if "gpt" in file else 0, "train", base_dir=f"{REUTER_PATH}/")
+trigram_model, tokenizer = train_trigram(return_tokenizer=True)
 
-# Generate Reuter Test Data
-reuter_test_data = reuter_generate_dataset(
-    t_featurize, "test", base_dir=f"{REUTER_PATH}/")
+wp_dataset = [
+    Dataset("normal", "writing_prompts/data/human"),
+    Dataset("normal", "writing_prompts/data/gpt"),
+    Dataset("normal", "writing_prompts/data/claude"),
+]
 
-davinci_logprobs, ada_logprobs, trigram_logprobs, unigram_logprobs = get_all_logprobs(
-    lambda featurize: reuter_generate_dataset(
-        featurize, "test", base_dir=f"{REUTER_PATH}/")
-)
-vector_map = {
-    "davinci-logprobs": lambda file: davinci_logprobs[file],
-    "ada-logprobs": lambda file: ada_logprobs[file],
-    "trigram-logprobs": lambda file: trigram_logprobs[file],
-    "unigram-logprobs": lambda file: unigram_logprobs[file]
-}
+reuter_dataset_train = [
+    Dataset("author", "reuter/data/human/train"),
+    Dataset("author", "reuter/data/gpt/train"),
+    Dataset("author", "reuter/data/claude/train"),
+]
+reuter_dataset_test = [
+    Dataset("author", "reuter/data/human/test"),
+    Dataset("author", "reuter/data/gpt/test"),
+    Dataset("author", "reuter/data/claude/test"),
+]
 
-reuter_t_test_data = reuter_generate_dataset(
-    t_featurize, "test", base_dir=f"{REUTER_PATH}/")
-reuter_exp_test_data = reuter_generate_dataset(
-    get_exp_featurize(vector_map, best_features), "test",
-    base_dir=f"{REUTER_PATH}/", verbose=True
-)
-reuter_test_data = np.concatenate(
-    (reuter_t_test_data, reuter_exp_test_data),
-    axis=1
-)
-reuter_test_labels = reuter_generate_dataset(
-    lambda file: 1 if "gpt" in file else 0, "test", base_dir=f"{REUTER_PATH}/")
-
-# Generate Essay Data
-essay_symbolic_data = pickle.load(open(f"essay/symbolic_data", "rb"))
-essay_t = essay_generate_dataset(
-    t_featurize, base_dir=f"{ESSAY_PATH}/", verbose=True)
-essay_data = np.concatenate(
-    [essay_symbolic_data[e]
-        for e in best_features] + [essay_t], axis=1
-)
-essay_labels = essay_generate_dataset(
-    lambda file: 1 if "gpt" in file else 0, base_dir=f"{ESSAY_PATH}/")
+essay_dataset = [
+    Dataset("normal", "essay/data/human"),
+    Dataset("normal", "essay/data/gpt"),
+    Dataset("normal", "essay/data/claude"),
+]
 
 
-# Combine data together
-data, mu, sigma = normalize(np.concatenate(
-    (wp_data, reuter_train_data, reuter_test_data, essay_data)), ret_mu_sigma=True)
-labels = np.concatenate((wp_labels, reuter_train_labels,
-                        reuter_test_labels, essay_labels))
+def get_featurized_data(generate_dataset_fn, best_features):
+    t_data = generate_dataset_fn(t_featurize)
 
-# Generate split
-indices = np.arange(data.shape[0])
-np.random.shuffle(indices)
-train, test = indices[:int(0.8 * len(indices))
-                      ], indices[int(0.8 * len(indices)):]
+    davinci, ada, trigram, unigram = get_all_logprobs(
+        generate_dataset_fn, trigram=trigram_model, tokenizer=tokenizer
+    )
 
-# Write train, test split to file
-with open("model/train.txt", "w") as f:
-    for i in train:
-        f.write(str(i) + "\n")
+    vector_map = {
+        "davinci-logprobs": lambda file: davinci[file],
+        "ada-logprobs": lambda file: ada[file],
+        "trigram-logprobs": lambda file: trigram[file],
+        "unigram-logprobs": lambda file: unigram[file],
+    }
+    exp_featurize = get_exp_featurize(best_features, vector_map)
+    exp_data = generate_dataset_fn(exp_featurize)
 
-with open("model/test.txt", "w") as f:
-    for i in test:
-        f.write(str(i) + "\n")
+    return np.concatenate([t_data, exp_data], axis=1)
 
-# Train model
-model = LogisticRegression(C=10, penalty="l2", max_iter=10000)
-model.fit(data[train], labels[train])
 
-# Evaluate model
-preds = model.predict(data[test])
-print("F1 Score:", f1_score(labels[test], preds))
-print("Accuracy:", accuracy_score(labels[test], preds))
-print("ROC AUC Score:", roc_auc_score(labels[test], preds))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generate_symbolic_data", action="store_true")
+    parser.add_argument("--perform_feature_selection", action="store_true")
+    parser.add_argument("--seed", type=int, default=0)
+    args = parser.parse_args()
 
-# Save model
-pickle.dump(model, open("model/model", "wb"))
-pickle.dump(mu, open("model/mu", "wb"))
-pickle.dump(sigma, open("model/sigma", "wb"))
+    np.random.seed(args.seed)
+
+    result_table = [["F1", "Accuracy", "AUC"]]
+
+    datasets = [
+        *wp_dataset,
+        *reuter_dataset_train,
+        *reuter_dataset_test,
+        *essay_dataset,
+    ]
+    generate_dataset_fn = get_generate_dataset(*datasets)
+
+    if args.generate_symbolic_data:
+        generate_symbolic_data(
+            generate_dataset_fn, max_depth=3, output_file="symbolic_data", verbose=True
+        )
+
+    if args.perform_feature_selection:
+        pass
+
+    data, mu, sigma = normalize(
+        get_featurized_data(generate_dataset_fn, best_features_all), ret_mu_sigma=True
+    )
+    labels = generate_dataset_fn(
+        lambda file: 1 if any([m in file for m in ["gpt", "claude"]]) else 0
+    )
+
+    indices = np.arange(len(data))
+    np.random.shuffle(indices)
+    train, test, valid = (
+        indices[: math.floor(0.8 * len(data))],
+        indices[math.floor(0.8 * len(data)) : math.floor(0.9 * len(data))],
+        indices[math.floor(0.9 * len(data)) :],
+    )
+
+    model = LogisticRegression(C=10, penalty="l2", max_iter=10000)
+    model.fit(data[train], labels[train])
+
+    predictions = model.predict(data[test])
+    probs = model.predict_proba(data[test])[:, 1]
+
+    result_table.append(
+        [
+            round(f1_score(labels[test], predictions), 3),
+            round(accuracy_score(labels[test], predictions), 3),
+            round(roc_auc_score(labels[test], probs), 3),
+        ]
+    )
+
+    print(tabulate(result_table, headers="firstrow", tablefmt="grid"))
--- utils/score.py
+++ utils/score.py
@@ -14,10 +14,10 @@ def k_fold_score(X, labels, indices=None, k=8, precision=3):
 
     score_sum = 0
     for i in range(k):
-        train = np.concatenate([np.array(k_split[j])
-                               for j in range(k) if i != j])
-        model = LogisticRegression(C=10, penalty='l2', max_iter=1000)
+        train = np.concatenate([np.array(k_split[j]) for j in range(k) if i != j])
+        model = LogisticRegression(C=10, penalty="l2", max_iter=1000)
         model.fit(X[train], labels[train])
+
         score_sum += model.score(X[k_split[i]], labels[k_split[i]])
 
     return round(score_sum / k, precision)
